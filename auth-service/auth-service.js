291b239df0b0f98777e4a4973f1f990701ecfda3
maxenceTroislouche <maxence62700@outlook.fr>
2024-12-10 14:27:09 +0100

    added UserId in request headers for micro services

--- auth-service/auth-service.js
+++ auth-service/auth-service.js
@@ -16,15 +16,25 @@ app.post('/login', (req, res) => {
 
 app.get('/validate', (req, res) => {
   const authHeader = req.headers['authorization'];
-  if(!authHeader) return res.status(401).send('No token provided');
+  if (!authHeader) {
+    return res.status(401).send('No token provided');
+  }
   
   const token = authHeader.replace('Bearer ', '');
-  
+
   jwt.verify(token, SECRET, (err, decoded) => {
-    if (err) return res.status(401).send('Invalid token');
+    if (err) {
+      return res.status(401).send('Invalid token');
+    }
+
+    // Une fois le token validé, on ajoute l'en-tête
+    // Supposons que decoded.userId soit défini
+    res.set("X-User-Id", 1);
+
     res.status(200).send('OK');
   });
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Auth service running on port ${port}`));
